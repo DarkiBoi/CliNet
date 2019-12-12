c626c6b6356415f3af0d23743ee2c1f773a12635
DarkiBoi <jw.winkelm.2005@gmail.com>
2019-12-12 19:11:01 +0100

    Added ChatAppend Modes
    WIP
    @asikesa gimme chat appends thx

--- KAMI/src/main/java/me/zeroeightsix/kami/module/modules/misc/CustomChat.java
+++ KAMI/src/main/java/me/zeroeightsix/kami/module/modules/misc/CustomChat.java
@@ -4,6 +4,8 @@ import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import me.zeroeightsix.kami.event.events.PacketEvent;
 import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.module.ModuleManager;
+import me.zeroeightsix.kami.module.modules.gui.Themes;
 import me.zeroeightsix.kami.setting.Setting;
 import me.zeroeightsix.kami.setting.Settings;
 import net.minecraft.network.play.client.CPacketChatMessage;
@@ -15,15 +17,41 @@ import net.minecraft.network.play.client.CPacketChatMessage;
 public class CustomChat extends Module {
 
     private Setting<Boolean> commands = register(Settings.b("Commands", false));
+    public static Setting<CustomChat.Mode> mode = Settings.e("Mode", Mode.CliNet);
 
+    private final String KAMI_SUFFIX = " \u23D0 \u1D0B\u1D00\u1D0D\u026A";
     private final String CN_SUFFIX = " | CliNet Beta";
+
+    private static CustomChat INSTANCE = new CustomChat();
+
+    public CustomChat() {
+        INSTANCE = this;
+        register(mode);
+    }
+
+    public enum Mode {
+        KAMI, CliNet
+    }
+
+    @Override
+    public void onDisable() {
+        Module module = ModuleManager.getModuleByName("CustomChat");
+        module.enable();
+    }
 
     @EventHandler
     public Listener<PacketEvent.Send> listener = new Listener<>(event -> {
         if (event.getPacket() instanceof CPacketChatMessage) {
             String s = ((CPacketChatMessage) event.getPacket()).getMessage();
             if (s.startsWith("/") && !commands.getValue()) return;
-            s += CN_SUFFIX;
+            switch (mode.getValue()) {
+                case KAMI:
+                    s += KAMI_SUFFIX;
+                    break;
+                case CliNet:
+                    s += CN_SUFFIX;
+                    break;
+            }
             if (s.length() >= 256) s = s.substring(0,256);
             ((CPacketChatMessage) event.getPacket()).message = s;
         }
