ae4888ec0d22b2a2e9f8d35a800bb4ec1d2c2ac0
Hamburger2k <59456376+Hamburger2k@users.noreply.github.com>
2020-01-13 11:43:14 +0100

    Fixed FastEXP
    It now only triggers on expbottles, I was retarded and had a semicolon behind my if-statement.

--- KAMI/src/main/java/me/zeroeightsix/kami/module/modules/combat/FastEXP.java
+++ KAMI/src/main/java/me/zeroeightsix/kami/module/modules/combat/FastEXP.java
@@ -4,11 +4,15 @@ import me.zeroeightsix.kami.module.Module;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemExpBottle;
 
-
+/*
+* Created by Hamburger on 13/01/2020
+*/
 
 @Module.Info(name = "FastEXP", category = Module.Category.COMBAT, description = "EXP bottles go fast")
 
 public class FastEXP extends Module {
+
+
 
     @Override
     public void onUpdate() {
@@ -16,17 +20,11 @@ public class FastEXP extends Module {
         Item main = mc.player.getHeldItemMainhand().getItem();
         Item off = mc.player.getHeldItemOffhand().getItem();
 
-        if ( main instanceof ItemExpBottle | off instanceof ItemExpBottle) ;
+        if(main instanceof ItemExpBottle | off instanceof ItemExpBottle)
         {
            mc.rightClickDelayTimer = 0;
         }
 
-
-
     }
-
-    @Override
-    public void onDisable() {
-        mc.rightClickDelayTimer = 4;
-    }
+    
 }
