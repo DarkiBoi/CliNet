e72f1040ab141567c573c5d8a7bd5cb9613a301f
Hamburger2k <59456376+Hamburger2k@users.noreply.github.com>
2020-01-22 17:00:11 +0100

    SnappleReply + Util
    get a random fact for every module you want by writing SnappleFacts.randomfact() ezpzpzpzpz

--- KAMI/src/main/java/me/zeroeightsix/kami/module/modules/misc/AutoReply.java
+++ KAMI/src/main/java/me/zeroeightsix/kami/module/modules/misc/AutoReply.java
@@ -4,6 +4,9 @@ import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import me.zeroeightsix.kami.event.events.PacketEvent;
 import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
+import me.zeroeightsix.kami.util.SnappleFacts;
 import net.minecraft.network.play.server.SPacketChat;
 import me.zeroeightsix.kami.command.commands.AutoReplyCommand;
 
@@ -15,13 +18,27 @@ import me.zeroeightsix.kami.command.commands.AutoReplyCommand;
 @Module.Info(name = "AutoReply", category = Module.Category.MISC, description = "automatically replies to messages with a custom message")
 public class AutoReply extends Module {
 
+    private Setting<Mode> mode = register(Settings.e("Mode", Mode.SNAPPLE));
+
+    public enum Mode {
+        CUSTOM, SNAPPLE
+    }
 
     @EventHandler
     private Listener<PacketEvent.Receive> receiveListener = new Listener<>(event -> {
         if (event.getPacket() instanceof SPacketChat) {
             String r = ((SPacketChat) event.getPacket()).getChatComponent().getUnformattedText();
             if (r.contains("whispers: ")) {
-                mc.player.sendChatMessage("/r" + AutoReplyCommand.areply);
+                String msg = "";
+
+                switch (mode.getValue()){
+                    case CUSTOM:
+                        msg = AutoReplyCommand.areply;
+                        break;
+                    case SNAPPLE:
+                        msg = SnappleFacts.randomfact();
+                }
+                mc.player.sendChatMessage("/r " + msg);
             }
 
         }
